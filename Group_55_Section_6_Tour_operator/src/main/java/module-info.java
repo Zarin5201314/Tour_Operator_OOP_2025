af32a90f5b22ad70b2148a9c01e7c8a3e720215c
Zarin5201314 <zarinsultan7@gmail.com>
2025-08-01 23:20:17 +0600

    init

---
+++ Group_55_Section_6_Tour_operator/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.midpracticeproblem.group_55_section_6_tour_operator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.midpracticeproblem.group_55_section_6_tour_operator to javafx.fxml;
+    exports com.midpracticeproblem.group_55_section_6_tour_operator;
+}
