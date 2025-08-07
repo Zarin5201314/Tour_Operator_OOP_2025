70ff2ac8cc90b010d948ce6507f274ecd6648794
Abdur Rahman <arsumon904@gmail.com>
2025-08-07 16:36:18 +0600

    Abdur Rahman

--- Group_55_Section_6_Tour_operator/src/main/java/module-info.java
+++ Group_55_Section_6_Tour_operator/src/main/java/module-info.java
@@ -4,5 +4,7 @@ module com.midpracticeproblem.group_55_section_6_tour_operator {
 
 
     opens com.midpracticeproblem.group_55_section_6_tour_operator to javafx.fxml;
+    opens com.midpracticeproblem.group_55_section_6_tour_operator.TourGuide to javafx.fxml, javafx.base;
+    opens com.midpracticeproblem.group_55_section_6_tour_operator.OperationManager to javafx.fxml, javafx.base;
     exports com.midpracticeproblem.group_55_section_6_tour_operator;
 }
