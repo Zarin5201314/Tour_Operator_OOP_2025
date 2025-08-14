8c457b356d1b81517c04bb568aa9c356aa5a37c7
Zarin5201314 <zarinsultan7@gmail.com>
2025-08-14 18:23:35 +0600

    Update CancelBookingController.java

--- Group_55_Section_6_Tour_operator/src/main/java/com/midpracticeproblem/group_55_section_6_tour_operator/Tourist/CancelBookingController.java
+++ Group_55_Section_6_Tour_operator/src/main/java/com/midpracticeproblem/group_55_section_6_tour_operator/Tourist/CancelBookingController.java
@@ -16,12 +16,60 @@ public class CancelBookingController
     private TableView cancelBookingTableView;
     @javafx.fxml.FXML
     private TableColumn packageIDColumn;
+    @javafx.fxml.FXML
+    private Label outPutLabel;
+
+    private ObservableList<TourPackages> bookingList = FXCollections.observableArrayList();
+    
 
     @javafx.fxml.FXML
     public void initialize() {
+        
+        
+        packageNamneColumn.setCellValueFactory(new PropertyValueFactory<>("packageName"));
+        bookingIDColumn.setCellValueFactory(new PropertyValueFactory<>("bookingID"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+        packageIDColumn.setCellValueFactory(new PropertyValueFactory<>("packageID"));
+
+
+
+        cancelBookingTableView.setItems(bookingList);
     }
 
     @javafx.fxml.FXML
     public void cancelBookingOnAction(ActionEvent actionEvent) {
+
+        TourPackages selectedBooking = cancelBookingTableView.getSelectionModel().getSelectedItem();
+
+        if (selectedBooking != null) {
+            if (selectedBooking.getStatus().equals("Cancelled")) {
+                outPutLabel.setText("This booking is already cancelled.");
+                return;
+            }
+
+            selectedBooking.setStatus("Cancelled");
+
+            cancelBookingTableView.refresh();
+            outPutLabel.setText("Booking ID " + selectedBooking.getBookingID() + " has been cancelled.");
+
+
+        } else {
+            outPutLabel.setText("Please select a booking to cancel.");
+        }
+
+        
     }
+
+
+    @javafx.fxml.FXML
+    public void goBackOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Tourist/TouristDashBoard.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Tourist Dashboard");
+        stage.show();
+    }
+
+
 }
