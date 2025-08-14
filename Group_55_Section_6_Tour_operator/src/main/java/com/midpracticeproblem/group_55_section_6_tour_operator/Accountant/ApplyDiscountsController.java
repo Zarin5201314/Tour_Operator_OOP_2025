5316b5d0bf0533c98893243e679d69c1d97cf91b
Zarin5201314 <zarinsultan7@gmail.com>
2025-08-14 19:04:11 +0600

    Update ApplyDiscountsController.java

--- Group_55_Section_6_Tour_operator/src/main/java/com/midpracticeproblem/group_55_section_6_tour_operator/Accountant/ApplyDiscountsController.java
+++ Group_55_Section_6_Tour_operator/src/main/java/com/midpracticeproblem/group_55_section_6_tour_operator/Accountant/ApplyDiscountsController.java
@@ -3,10 +3,13 @@ package com.midpracticeproblem.group_55_section_6_tour_operator.Accountant;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 
+
 public class ApplyDiscountsController
 {
     @javafx.fxml.FXML
     private TextField discountTextField;
+    @javafx.fxml.FXML
+    private Label outPutLabel;
 
     @javafx.fxml.FXML
     public void initialize() {
@@ -14,5 +17,43 @@ public class ApplyDiscountsController
 
     @javafx.fxml.FXML
     public void applyDiscountOnAction(ActionEvent actionEvent) {
+        String discountText = discountTextField.getText();
+
+        // --- Input Validation ---
+        if (discountText.isBlank()) {
+            outPutLabel.setText("Please enter a discount percentage.");
+            return;
+        }
+
+        try {
+            double discount = Double.parseDouble(discountText);
+
+            if (discount < 0 || discount > 100) {
+                outPutLabel.setText("Discount must be between 0 and 100%.");
+                return;
+            }
+
+
+            System.out.println("--- Applying Discount ---");
+            System.out.println("Applying a " + discount + "% discount.");
+            System.out.println("-------------------------");
+
+            outPutLabel.setText(String.format("Successfully applied %.2f%% discount!", discount));
+
+            discountTextField.clear();
+
+        } catch (NumberFormatException e) {
+            outPutLabel.setText("Please enter a valid number for the discount.");
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void cancelOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Accountant/AccountantDashBoard.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Accountant DashBoard");
+        stage.show();
     }
 }
