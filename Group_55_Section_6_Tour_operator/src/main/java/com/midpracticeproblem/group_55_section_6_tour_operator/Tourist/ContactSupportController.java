8e09078b87b2c5cb3fd08e6197022d77bcd687bc
Zarin5201314 <zarinsultan7@gmail.com>
2025-08-14 18:25:45 +0600

    Update ContactSupportController.java

--- Group_55_Section_6_Tour_operator/src/main/java/com/midpracticeproblem/group_55_section_6_tour_operator/Tourist/ContactSupportController.java
+++ Group_55_Section_6_Tour_operator/src/main/java/com/midpracticeproblem/group_55_section_6_tour_operator/Tourist/ContactSupportController.java
@@ -10,6 +10,8 @@ public class ContactSupportController
     private TextField subjectTextField;
     @javafx.fxml.FXML
     private TextArea messageTextArea;
+    @javafx.fxml.FXML
+    private Label outPutLabel;
 
     @javafx.fxml.FXML
     public void initialize() {
@@ -17,9 +19,36 @@ public class ContactSupportController
 
     @javafx.fxml.FXML
     public void SendOnAction(ActionEvent actionEvent) {
+        String subject = subjectTextField.getText();
+        String message = messageTextArea.getText();
+
+        //  Validation ---
+        if (subject.isBlank() || message.isBlank()) {
+            outPutLabel.setText("Subject and Message fields cannot be empty.");
+            return;
+        }
+
+        
+        System.out.println("--- Sending Support Request ---");
+        System.out.println("Subject: " + subject);
+        System.out.println("Message: " + message);
+        System.out.println("-------------------------------");
+
+        outPutLabel.setText("Your message has been sent to support!");
+        // Optionally, clear fields after sending
+        subjectTextField.clear();
+        messageTextArea.clear();
     }
 
     @javafx.fxml.FXML
-    public void cancelOnAction(ActionEvent actionEvent) {
+    public void cancelOnAction(ActionEvent actionEvent)throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Tourist/TouristDashBoard.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Tourist Dashboard");
+        stage.show();
+
+        
     }
 }
